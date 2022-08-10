db90dab3c02916348981827e532a4f41de75a853
Karoline Ribeiro Fronza <karoline.fronza@hotmail.com>
2022-08-10 19:52:19 -0300

    Aula - componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
         <Header />
 
-       <Post 
-          author="Karoline Fronza" 
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus accusantium error quasi quas tenetur exercitationem reiciendis recusandae consequuntur temporibus et repellendus veniam aspernatur quia, aperiam quos officia saepe iste quod" 
-        />
-        <Post 
-          author="João"
-          content="Novo post!!"
-        />
+       <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+
+            <Post 
+               author="Karoline Fronza" 
+               content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus accusantium error quasi quas tenetur exercitationem reiciendis recusandae consequuntur temporibus et repellendus veniam aspernatur quia, aperiam quos officia saepe iste quod" 
+           />
+             <Post 
+              author="João"
+               content="Novo post!!"
+            />
+
+        </main>
+       </div>
     </div>
   )
 }
