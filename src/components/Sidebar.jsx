ce71ab84f97081aa30dc03a00adc1d5bb20c207e
Karoline Ribeiro Fronza <karoline.fronza@hotmail.com>
2022-08-11 21:06:44 -0300

    Finalizado a sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 //exportando um componente chamado Sidebar
@@ -9,13 +11,16 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
         
-            <div className={styles.profile}>
+            <div className={styles.profile}> 
+                <img className={styles.avatar} src="https://github.com/karolfronza.png" />
+
                 <strong>Karoline Fronza</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">
+                <a href="#"> 
+                    <PencilLine size={20} /> 
                     Editar seu perfil
                 </a>
             </footer>
